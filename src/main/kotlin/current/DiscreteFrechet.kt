ea3257f198b46fde2b2544cf0b6aadd0adaffab8
Yvee1 <stevenvdb@live.nl>
2021-09-12 23:19:42 +0200

    auto commit

--- src/main/kotlin/current/DiscreteFrechet.kt
+++ src/main/kotlin/current/DiscreteFrechet.kt
@@ -3,15 +3,20 @@ package current
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.*
+import org.openrndr.drawComposition
 import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.olive.oliveProgram
 import org.openrndr.extras.color.presets.BLUE_STEEL
 import org.openrndr.extras.color.presets.ORANGE
 import org.openrndr.math.Vector2
+import org.openrndr.shape.CompositionDrawer
 import org.openrndr.shape.LineSegment
 import org.openrndr.shape.Rectangle
+import org.openrndr.svg.saveToFile
 import useful.NativeGitArchiver
 import useful.TransRotScale
 import useful.Viridis
+import java.io.File
 import kotlin.math.max
 import kotlin.math.min
 
@@ -180,32 +185,41 @@ fun main() {
         }
 
         program {
-            fun drawPolyline(polyline: List<Vector2>){
-                drawer.apply {
-                    fill = ColorRGBa.WHITE
-                    lineStrip(polyline)
-                    strokeWeight /= 2.0
-                    circles(polyline, 0.05)
-                    strokeWeight *= 2.0
-                }
+            fun Drawer.drawPolyline(polyline: List<Vector2>){
+                fill = ColorRGBa.WHITE
+                lineStrip(polyline)
+                strokeWeight /= 2.0
+                circles(polyline, 0.05)
+                strokeWeight *= 2.0
             }
 
-            fun drawMapping(mapping: List<Pair<Int, Int>>, p: List<Vector2>, q: List<Vector2>){
-                drawer.apply {
-                    stroke = ColorRGBa.PINK
-                    lineSegments(mapping.map { pair -> LineSegment(p[pair.first], q[pair.second]) })
-                }
+            fun CompositionDrawer.drawPolyline(polyline: List<Vector2>){
+                fill = ColorRGBa.WHITE
+                lineStrip(polyline)
+                strokeWeight /= 2.0
+                circles(polyline, 0.05)
+                strokeWeight *= 2.0
+            }
+
+            fun Drawer.drawMapping(mapping: List<Pair<Int, Int>>, p: List<Vector2>, q: List<Vector2>){
+                stroke = ColorRGBa.PINK
+                lineSegments(mapping.map { pair -> LineSegment(p[pair.first], q[pair.second]) })
+            }
+
+            fun CompositionDrawer.drawMapping(mapping: List<Pair<Int, Int>>, p: List<Vector2>, q: List<Vector2>){
+                stroke = ColorRGBa.PINK
+                lineSegments(mapping.map { pair -> LineSegment(p[pair.first], q[pair.second]) })
             }
 
             val viridis = Viridis()
-            fun drawDPTable(table: Array<Array<Double>>, pos: Vector2, size: Double, mapping: List<Pair<Int, Int>>? = null){
+            fun Drawer.drawDPTable(table: Array<Array<Double>>, pos: Vector2, size: Double){
                 val n = table.size
                 val m = table[0].size
 
                 val cellSize = size / max(n, m)
                 val largest: Double = table.map { it.maxOrNull()!! }.maxOrNull()!!
 
-                val rBatch = drawer.rectangleBatch {
+                val rBatch = rectangleBatch {
                     stroke = null
                     for (i in 0 until n) {
                         for (j in 0 until m) {
@@ -218,7 +232,7 @@ fun main() {
                     }
                 }
 
-                drawer.isolated {
+                isolated {
                     translate(pos)
 
                     // Colored cells
@@ -241,14 +255,72 @@ fun main() {
 
                     // Outer rectangle
                     contour(Rectangle(0.0, 0.0, w, h).contour)
-
-                    mapping?.let {
-                        stroke = ColorRGBa.RED
-                        lineStrip(it.map { pair -> Vector2((pair.second+0.5)  * cellSize, (pair.first+0.5) * cellSize) })
-                    }
                 }
             }
 
+            fun CompositionDrawer.drawDPTable(table: Array<Array<Double>>, pos: Vector2, size: Double){
+                val n = table.size
+                val m = table[0].size
+
+                val cellSize = size / max(n, m)
+                val largest: Double = table.map { it.maxOrNull()!! }.maxOrNull()!!
+
+                isolated {
+                    translate(pos)
+
+                    // Colored cells
+                    stroke = null
+                    for (i in 0 until n) {
+                        for (j in 0 until m) {
+                            val t = table[i][j]/largest
+//                            fill = ColorRGBa.BLACK.opacify(1-t)
+                            fill = viridis.getColor(t)
+                            val r = Rectangle(j * cellSize, i * cellSize, cellSize, cellSize) as Rectangle
+                            rectangle(r)
+                        }
+                    }
+
+                    stroke = ColorRGBa.BLACK
+                    fill = null
+                    val w = m*cellSize
+                    val h = n*cellSize
+
+                    // Grid lines
+                    strokeWeight /= 2.0
+                    for (i in 1 until n) {
+                        lineSegment(0.0, i * cellSize, w, i * cellSize)
+                    }
+                    for (j in 1 until m) {
+                        lineSegment(j * cellSize, 0.0, j*cellSize, h)
+                    }
+                    strokeWeight *= 2.0
+
+                    // Outer rectangle
+                    contour(Rectangle(0.0, 0.0, w, h).contour)
+                }
+            }
+
+            fun Drawer.drawDPPath(table: Array<Array<Double>>, pos: Vector2, size: Double, mapping: List<Pair<Int, Int>>){
+                val n = table.size
+                val m = table[0].size
+                val cellSize = size / max(n, m)
+
+                isolated {
+                    translate(pos)
+                    lineStrip(mapping.map { pair -> Vector2((pair.second+0.5)  * cellSize, (pair.first+0.5) * cellSize) })
+                }
+            }
+
+            fun CompositionDrawer.drawDPPath(table: Array<Array<Double>>, pos: Vector2, size: Double, mapping: List<Pair<Int, Int>>){
+                val n = table.size
+                val m = table[0].size
+                val cellSize = size / max(n, m)
+
+                isolated {
+                    translate(pos)
+                    lineStrip(mapping.map { pair -> Vector2((pair.second+0.5)  * cellSize, (pair.first+0.5) * cellSize) })
+                }
+            }
 
             val p = listOf(1.0 v2 0.0, 2.0 v2 0.5, 2.0 v2 -0.5)
             val q = listOf(0.5 v2 0.0, 0.6 v2 -0.2, 0.7 v2 -0.35, 0.8 v2 -0.45, 1.0 v2 -0.5, 1.3 v2 -0.35)
@@ -275,38 +347,101 @@ fun main() {
                 if (i == row.size-1) println()
             }
 
+            val comp = drawComposition {
+                translate(width/2.0, height/2.0)
+                scale(100.0)
+                scale(1.0, -1.0)
+//                    translate(-2.5, 0.0)
+//                strokeWeight /= 50.0
+
+                isolated {
+                    translate(0.0, -1.5)
+                    strokeWeight /= 2.0
+                    drawMapping(mapping, p, q)
+                    strokeWeight *= 2.0
+                    stroke = ColorRGBa.BLACK
+//                    stroke = ColorRGBa.ORANGE
+                    drawPolyline(p)
+//                    stroke = ColorRGBa.BLUE_STEEL
+                    drawPolyline(q)
+                }
+
+                val pos = Vector2(-5.0, -2.0)
+                val size = 5.0
+                drawDPTable(gamma, pos, size)
+                isolated {
+                    stroke = ColorRGBa.fromHex("E7ECEF")
+                    translate(0.0, -0.01)
+                    drawDPPath(gamma, pos, size, mapping)
+                }
+                isolated {
+//                        stroke = ColorRGBa.fromHex("390040")
+                    stroke = ColorRGBa.fromHex("FF8811")
+                    translate(0.0, 0.01)
+                    drawDPPath(gamma, pos, size, mappingLimited)
+                }
+
+                isolated {
+                    translate(0.0, 0.25)
+                    strokeWeight /= 2.0
+                    drawMapping(mappingLimited, p, q)
+                    strokeWeight *= 2.0
+                    stroke = ColorRGBa.BLACK
+//                    stroke = ColorRGBa.ORANGE
+                    drawPolyline(p)
+//                    stroke = ColorRGBa.BLUE_STEEL
+                    drawPolyline(q)
+                }
+            }
+            comp.saveToFile(File("teeeeeeesssssttttt.svg"))
+
             extend(NativeGitArchiver())
             extend(Screenshots())
             extend(TransRotScale())
             extend {
-                drawer.apply {
-                    clear(ColorRGBa.WHITE)
-                    translate(width/2.0, height/2.0)
-                    scale(100.0)
-                    scale(1.0, -1.0)
-//                    translate(-2.5, 0.0)
-                    strokeWeight /= 50.0
-
-                    strokeWeight /= 2.0
-                    drawMapping(mapping, p, q)
-                    strokeWeight *= 2.0
-                    stroke = ColorRGBa.ORANGE
-                    drawPolyline(p)
-                    stroke = ColorRGBa.BLUE_STEEL
-                    drawPolyline(q)
-
-                    drawDPTable(gamma, Vector2(-5.0, -2.0), 5.0, mapping)
-
-                    translate(0.0, 2.0)
-                    strokeWeight /= 2.0
-                    drawMapping(mappingLimited, p, q)
-                    strokeWeight *= 2.0
-
-                    stroke = ColorRGBa.ORANGE
-                    drawPolyline(p)
-                    stroke = ColorRGBa.BLUE_STEEL
-                    drawPolyline(q)
-                }
+                drawer.clear(ColorRGBa.WHITE)
+                drawer.composition(comp)
+//                drawer.apply {
+//                    clear(ColorRGBa.WHITE)
+//                    translate(width/2.0, height/2.0)
+//                    scale(100.0)
+//                    scale(1.0, -1.0)
+////                    translate(-2.5, 0.0)
+//                    strokeWeight /= 50.0
+//
+//                    strokeWeight /= 2.0
+//                    drawMapping(mapping, p, q)
+//                    strokeWeight *= 2.0
+//                    stroke = ColorRGBa.ORANGE
+//                    drawPolyline(p)
+//                    stroke = ColorRGBa.BLUE_STEEL
+//                    drawPolyline(q)
+//
+//                    val pos = Vector2(-5.0, -2.0)
+//                    val size = 5.0
+//                    drawDPTable(gamma, pos, size)
+//                    isolated {
+//                        stroke = ColorRGBa.fromHex("E7ECEF")
+//                        translate(0.0, -strokeWeight / 2)
+//                        drawDPPath(gamma, pos, size, mapping)
+//                    }
+//                    isolated {
+////                        stroke = ColorRGBa.fromHex("390040")
+//                        stroke = ColorRGBa.fromHex("FF8811")
+//                        translate(0.0, strokeWeight/2)
+//                        drawDPPath(gamma, pos, size, mappingLimited)
+//                    }
+//
+//                    translate(0.0, 2.0)
+//                    strokeWeight /= 2.0
+//                    drawMapping(mappingLimited, p, q)
+//                    strokeWeight *= 2.0
+//
+//                    stroke = ColorRGBa.ORANGE
+//                    drawPolyline(p)
+//                    stroke = ColorRGBa.BLUE_STEEL
+//                    drawPolyline(q)
+//                }
             }
         }
     }
